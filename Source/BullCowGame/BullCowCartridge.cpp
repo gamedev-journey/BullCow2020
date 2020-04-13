395537ef3abb8e94cc41887a8b04c08ce4fb8ca8
AzraelleStudios <azraellestudio@gmail.com>
2020-04-13 15:31:10 +0100

    no message

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -6,11 +6,30 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bulls and Cows\n"));
+    PrintLine(TEXT("Guess the 4 letter word."));
     PrintLine(TEXT("Press enter to continue..."));
+    HiddenWord = TEXT("coal");
+
+    // set lives
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("coal");
+    
+
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("Well done"));
+    }
+    else
+    {
+        PrintLine(TEXT("Better luck next time"));
+
+       
+    }
+
+    // check if isogram
+    // check number of characters
+     // remove life
 }   
