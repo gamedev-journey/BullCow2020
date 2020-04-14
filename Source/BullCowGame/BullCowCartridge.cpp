13469a3ece657a24b4dc4295894a2031d3a08c02
AzraelleStudios <azraellestudio@gmail.com>
2020-04-14 20:37:24 +0100

    end of lecture 61 - switch to VS community

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,38 +5,67 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("Welcome to Bulls and Cows\n"));
-    PrintLine(TEXT("Guess the 4 letter word."));
-    PrintLine(TEXT("Press enter to continue..."));
-    
     
     SetupGame();
+
+    
     
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
-{
-    ClearScreen();
-    
-
-    if (Input == HiddenWord)
-    {
-        PrintLine(TEXT("Well done"));
-    }
-    else
-    {
-        PrintLine(TEXT("Better luck next time"));
-
-       
-    }
-
-    // check if isogram
-    // check number of characters
-     // remove life
+{   
+    ProcessGuess(Input);
 }   
 
 void UBullCowCartridge::SetupGame()
 {
     HiddenWord = TEXT("coal");
-    Lives = 5;
+    Lives = HiddenWord.Len();
+    bGameOver = false;
+    PrintLine(TEXT("Welcome to Bulls and Cows\n"));
+    PrintLine(TEXT("Guess the %i letter word."), HiddenWord.Len());
+    PrintLine(TEXT("Lives: %i"), Lives);
+    PrintLine(TEXT("Type your answer\nand press enter to continue."));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Please press enter to play again."));
+}
+
+void UBullCowCartridge::ProcessGuess(FString Guess)
+{
+    if (bGameOver)
+    {
+        ClearScreen();
+        SetupGame();
+    }
+    else
+    {
+
+        if (Guess == HiddenWord)
+        {
+            PrintLine(TEXT("Well done"));
+            EndGame();
+        }
+        else
+        {
+            if (Guess.Len() != HiddenWord.Len())
+            {
+                PrintLine(TEXT("You are looking for a %i letter word."), HiddenWord.Len());
+            }
+
+            --Lives;
+            if (Lives > 0)
+            {
+                PrintLine(TEXT("Please try again. Lives: %i"), Lives);
+            }
+            else
+            {
+                PrintLine(TEXT("You have no lives left."));
+                EndGame();
+            }
+        }
+    }
 }
