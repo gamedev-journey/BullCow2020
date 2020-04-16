01776576deb29159795153d9c5c81f0aea677a76
AzraelleStudios <azraellestudio@gmail.com>
2020-04-16 13:24:53 +0100

    end of section - game complete - added quit option

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,31 +5,35 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    
+   
     Isogram = GetValidWords(Words);
     SetupGame();
-
-    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {   
-    ProcessGuess(Input);
+    if (Input == "quit")
+    {
+        GIsRequestingExit = true;
+    }
+    else
+    {
+        ProcessGuess(Input);
+    }
 }   
 
 void UBullCowCartridge::SetupGame()
 {
     HiddenWord = Isogram[FMath::RandRange(0, Isogram.Num() - 1)];
-    Lives = HiddenWord.Len();
+    Lives = HiddenWord.Len() * 2;
     bGameOver = false;
     PrintLine(TEXT("Welcome to Bulls and Cows\n"));
     PrintLine(TEXT("Guess the %i letter word."), HiddenWord.Len());
     PrintLine(TEXT("Lives: %i"), Lives);
-    PrintLine(TEXT("Type your answer\nand press enter to continue."));
-    PrintLine(TEXT("The word is %s"), *HiddenWord);
+    PrintLine(TEXT("Press tab and yype your answer.\nPress enter to continue."));
+    PrintLine(TEXT("Type quit to close the game."));
     
-
-
+    //PrintLine(TEXT("The word is %s"), *HiddenWord);
 }
 
 void UBullCowCartridge::EndGame()
@@ -46,7 +50,6 @@ void UBullCowCartridge::ProcessGuess(const FString& Guess)
         SetupGame();
     }
     
-
     if (Guess == HiddenWord)
     {
         PrintLine(TEXT("Well done"));
@@ -66,7 +69,6 @@ void UBullCowCartridge::ProcessGuess(const FString& Guess)
         return;
     }
 
-
     --Lives;
     if (Lives <= 0)
     {
@@ -76,6 +78,10 @@ void UBullCowCartridge::ProcessGuess(const FString& Guess)
         EndGame();
     }
     
+    FBullCowCount Score = GetBullCows(Guess);
+
+    PrintLine(TEXT("You have %i Bulls and %i Cows"), Score.Bulls, Score.Cows);
+
     PrintLine(TEXT("Please try again. Lives: %i"), Lives);
 }
 
@@ -92,9 +98,7 @@ bool UBullCowCartridge::IsIsogram(const FString& Word) const
             }
         }
     }
-    
     return true;
-    
 }
 
 TArray<FString> UBullCowCartridge::GetValidWords(const TArray<FString>& WordList) const
@@ -109,6 +113,31 @@ TArray<FString> UBullCowCartridge::GetValidWords(const TArray<FString>& WordList
         }
     }
     return ValidWords;
+}
+
+
+FBullCowCount UBullCowCartridge::GetBullCows(const FString& Guess) const
+{
+    FBullCowCount Count;
+
+    for (int32 GuessIndex = 0; GuessIndex < Guess.Len(); GuessIndex++)
+    {
+        if (Guess[GuessIndex] == HiddenWord[GuessIndex])
+        {
+            Count.Bulls++;
+            continue;
+        }
+
+        for (int32 HiddenIndex = 0; HiddenIndex < HiddenWord.Len(); HiddenIndex++)
+        {
+            if (Guess[GuessIndex] == HiddenWord[HiddenIndex])
+            {
+                Count.Cows++;
+                break;
+            }
+        }
+    }
+    return Count;
 }
 
 
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -6,6 +6,11 @@
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
@@ -20,6 +25,7 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	void ProcessGuess(const FString& Guess);
 	bool IsIsogram(const FString& Word) const;
 	TArray<FString> GetValidWords(const TArray<FString>&) const;
+	FBullCowCount GetBullCows(const FString& Guess) const;
 
 	// Your declarations go below!
 	private:
